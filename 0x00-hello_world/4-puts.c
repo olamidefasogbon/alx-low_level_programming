5f66370840e1ee47fd1c76cdc01604f8509ad07d
Olamide <successolamide46@gmail.com>
2023-02-16 16:38:34 -0800

    Add: 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- *main - This is a function
+ * main - This is a function
  *
- * return - always 0
+ * Return: always 0
  **/
 
 int main(void)
 {
 	puts("\""Programming is like building a multilingual puzzle);
-	return(0)
+	return(0);
 }
