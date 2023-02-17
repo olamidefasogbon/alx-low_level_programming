fe5aec3ded218a1f4e4b2d1bcecc33907ffe3c4b
Olamide <successolamide46@gmail.com>
2023-02-16 17:22:36 -0800

    Add: 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,11 @@
 /**
  * main - This is the main function
  *
- * Return:  0 (constant) 
- * */
+ * Return:  0 constant
+ */
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,")
-		return (0)
+	return (0)
 }
