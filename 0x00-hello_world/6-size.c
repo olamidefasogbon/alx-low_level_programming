78b5ed57d538c9c5500b2e1701a5acb4cb32d579
Olamide <successolamide46@gmail.com>
2023-02-16 18:35:12 -0800

    Add: 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,8 +12,6 @@ int main(void)
 	printf("size of an int: %lu byte(s)\n", sizeof(int));
 	printf("size of a long int: %lu byte(s)\n", sizeof(long int));
 	printf("size of a long long int: %llu byte(s)\n", sizeof(long long int));
-	printf("size of a float: %lu byte(s)\n", sizeof(float));
-	
-
+	printf("size of a float: %lu byte(s)\n", sizeof(float));	
 	return (0);
 }
