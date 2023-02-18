6646ae943ed0226e8862106be55f6bec11a6731d
Olamide <successolamide46@gmail.com>
2023-02-18 04:01:45 -0800

    Add: 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - main function in this program
+ *
+ * Return: 0(Success)
+ */
+int main(void)
+{
+	for(i = 'a' ; i <= 'z' ; i++)
+	putchar(i);
+	putchar("\n");
+	return (0);
+}
