31ec51ac5258b43c3984fdf7d1b6fc67beb03cbe
Olamide <successolamide46@gmail.com>
2023-02-18 12:01:14 -0800

    Add: 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,12 +7,17 @@
 int main(void)
 {
 	int i, j;
+	int first_pair = 1; /* flag set to true for first pair*/
 
 	for (i = 0; i < 99; i++)
 	{
 		for (j = i + 1; j < 99; j++)
 		{
-			if (i != j)
+			if (first_pair)
+			{
+				first_pair = 0; /*set flag to false*/
+			}
+			else if (i != j)
 			{
 				if (i != 98 || j != 99)
 				{
