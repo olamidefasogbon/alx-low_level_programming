0dd72aeb86360875b6862a56c077403ed09468ad
Olamide <successolamide46@gmail.com>
2023-02-18 12:08:46 -0800

    Add: 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,9 +9,9 @@ int main(void)
 	int i;
 	int j;
 
-	for (i = 0; i < 99; i++)
+	for (i = 0; i < 100; i++)
 	{
-		for (j = i + 1; j < 99; j++)
+		for (j = i + 1; j < 100; j++)
 		{
 			if (i != j)
 			{
