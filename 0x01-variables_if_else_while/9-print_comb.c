c036bf45ed37c53a66107a144f1d5a3699d67adc
Olamide <successolamide46@gmail.com>
2023-02-18 07:12:45 -0800

    Add: 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,21 +7,16 @@
 int main(void)
 {
 	int i;
-	int j;
 
 	for (i = 0; i < 10; i++)
-		for (j = i; j < 10; j++)
+	{
+		putchar(i + '0');
+		if (i != 9)
 		{
-			if (i != j)
-			{
-				putchar(i + '0');
-				putchar(j + '0');
-				if (i != 8 || j != 9)
-				{
-					putchar(',');
-				}
-			}
+			putchar(',');
+			putchar(' ');
 		}
+	}
 	putchar('\n');
 	return (0);
 }
