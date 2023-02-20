9599e5000ad16d3b6594e07bb080d269372588a7
Olamide <successolamide46@gmail.com>
2023-02-20 09:44:18 -0800

    Add: 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,36 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
- * main - prints all possible different combinations of three digits.
- * Return: Always 0 (success)
+ * main - returns a combination of number
+ *
+ * Return: return 0 if successful
  */
 int main(void)
 {
-        int ones = '0';
-        int tens = '0';
-        int hundreds = '0';
+int i, j;
 
-        for (hundreds = '0'; hundreds <= '9'; hundreds++)
-        {
-                for (tens = '0'; tens <= '9'; tens++)
-                {
-                        for (ones = '0'; ones <= '9'; ones++)
-                        {
-                                if (!((ones == tens) || (tens == hundreds) ||
-                                                        (tens > ones) || (hundreds > tens))) /* eliminates repitition*/
-                                {
-                                        putchar(hundreds);
-                                        putchar(tens);
-                                        putchar(ones);
-                                        if (!(ones == '9' && hundreds == '7' &&
-                                                                tens == '8')) /* adds commas and spaces */
-                                        {
-                                                putchar(',');
-                                                putchar(' ');
-                                        }
-                                }
-                        }
-                }
-        }
-        putchar('\n');
-        return (0);
+for (i = 0; i < 100; i++)
+{
+	for (j = 0; j < 100; j++)
+	{
+		if (i < j)
+		{
+			putchar((i / 10) + 48);
+			putchar((i % 10) + 48);
+			putchar(' ');
+			putchar((j / 10) + 48);
+			putchar((j % 10) + 48);
+			if (i != 98 || j != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
