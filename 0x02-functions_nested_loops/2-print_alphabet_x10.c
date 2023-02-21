d7fb7d84980da847f6d9df256d6e7f666921daa0
Olamide <successolamide46@gmail.com>
2023-02-21 15:28:24 -0800

    Add: 2-aplhabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabet in lowercase 10x
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char j;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
