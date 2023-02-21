07af97e29e971eb5ce3b56c8b1d7211c72c2ce6c
Olamide <successolamide46@gmail.com>
2023-02-21 14:33:03 -0800

    Add: 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,19 @@
-#include <stdio.h>
-#include <main.h>
+#include "main.h"
 /**
- * main - prints the required output and a new line
- *
+ * main - main void function
+ * _putchar - prints the required output _putchar and a new line
  * Return: 0 (success)
  */
 int main(void)
 {
-printf("_putchar\n");
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
