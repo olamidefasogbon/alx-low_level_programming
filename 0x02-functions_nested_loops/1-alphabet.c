84fcf2817afb589c1bcfc5f5231e159621b70708
Olamide <successolamide46@gmail.com>
2023-02-21 14:52:03 -0800

    Add: 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_alphabet - function prints alphabet in lowercase
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i='a'; i<='z'; i++)
+		_putchar(i);
+	_putchar('\n');
+}
