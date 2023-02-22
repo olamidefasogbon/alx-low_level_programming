9df4fb7fb4a056d8e98d783c4801032350c1d7d4
Olamide <successolamide46@gmail.com>
2023-02-21 16:23:13 -0800

    Add: 4-isaplha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isalpha - check if the inout is an aplhabet
+ *
+ * @c: function parameter
+ *
+ * Return: 1 (if it's an aplhabet) and 0 (if otherwise)
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 41 && c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
