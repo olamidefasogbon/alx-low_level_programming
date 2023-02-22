dfd5d5e45350c4a9839a2a31585a37d2a228c22c
Olamide <successolamide46@gmail.com>
2023-02-21 20:25:47 -0800

    Add: 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,22 +1,25 @@
 #include "main.h"
+#include <stdio.h>
 /**
- *
+ * print_sign - prints signs
+ * @n: function parameter
+ * Return: 1, -1, 0 (success)
  */
 int print_sign(int n)
 {
-	
 	if (n > 0)
 	{
 		_putchar('+');
-		_putchar(',');
-		_putchar(' ');
 		return (1);
 	}
 	else if (n == 0)
 	{
 		_putchar('0');
-		_putchar(',');
-		_putchar(' ');
 		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
 	}
 }
