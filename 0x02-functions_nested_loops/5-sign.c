954af4f33b21ef7e60c63314ea986d3773fd8795
Olamide <successolamide46@gmail.com>
2023-02-21 16:46:13 -0800

    add: unfinished

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *
+ */
+int print_sign(int n)
+{
+	
+	if (n > 0)
+	{
+		_putchar('+');
+		_putchar(',');
+		_putchar(' ');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		return (0);
+	}
+}
