f3325a1f1843b856bb0ea486ca822ced8e83fc8a
Olamide <successolamide46@gmail.com>
2023-02-22 14:13:32 -0800

    Add: 11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - function that prints required output after loop iteration
+ *
+ * @n: function parameter
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	if ((n >= 0) && (n <= 98))
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
+		}
+		printf("\n");
+	}
+	else if (n < 0)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
+		}
+		printf("\n");
+	}
+	else if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
+		}
+		printf("\n");
+	}
+	else if (n == 98)
+	{
+		printf("%d", n);
+		if (n != 98)
+			printf(", ");
+	}
+}
