dc9eb80761393a3f717c10f0035a8f4c5530b7a0
Olamide <successolamide46@gmail.com>
2023-02-22 10:13:41 -0800

    Add: 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * jack_bauer - function that prints every minute of the day of jack bauer
+ */
+
+void jack_bauer(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 60; j++)
+		{
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(':');
+			_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
