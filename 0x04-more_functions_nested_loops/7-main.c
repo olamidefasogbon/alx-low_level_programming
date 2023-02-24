cdc46568739671c0c70dc55eb6bbc7540c6a5de1
Olamide <successolamide46@gmail.com>
2023-02-23 16:36:51 -0800

    Add: 7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - checks the code
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	print_diagonal(0);
+	print_diagonal(2);
+	print_diagonal(10);
+	print_diagonal(-4);
+	return (0);
+}
