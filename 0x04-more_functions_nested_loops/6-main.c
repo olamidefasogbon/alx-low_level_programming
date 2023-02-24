cf0dbb739fcc268f80e74866fef85082ce0d5087
Olamide <successolamide46@gmail.com>
2023-02-23 16:12:51 -0800

    Add: 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	print_line(0);
+	print_line(2);
+	print_line(10);
+	print_line(-4);
+	return (0);
+}
