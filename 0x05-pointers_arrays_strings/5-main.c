65b77678fcdd8912adbb2a0c0f18121e57ea8b9a
Olamide <successolamide46@gmail.com>
2023-02-28 03:32:45 -0800

    Add: 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-main.c
+++ 0x05-pointers_arrays_strings/5-main.c
@@ -9,6 +9,7 @@
 int main(void)
 {
 	char s[10] = "My School";
+
 	printf("%s\n", s);
 	rev_string(s);
 	printf("%s\n", s);
--- 0x05-pointers_arrays_strings/6-main.c
+++ 0x05-pointers_arrays_strings/6-main.c
@@ -9,6 +9,7 @@
 int main(void)
 {
 	char *str;
+
 	str = "0123456789";
 	puts2(str);
 	return (0);
