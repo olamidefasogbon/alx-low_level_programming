48f3bb8dee411ebe9ff71a4dc74f739ddece3e55
Olamide <successolamide46@gmail.com>
2023-03-10 11:49:22 -0800

    Add: 3-puts.c

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+void _puts(char *str);
+#include <stdio.h>
+/**
+ * main - Entry point
+ * _puts - puts in a string
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
