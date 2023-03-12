d50fd08b17953293ea792072b6698e099b13728f
Olamide <successolamide46@gmail.com>
2023-03-12 14:31:07 -0700

    Added: main.c

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - prints the given output
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+	return (0);
+}
