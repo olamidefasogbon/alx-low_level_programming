9be8cb3ab3f52e40a68cd95d981af257877a1183
Olamide <successolamide46@gmail.com>
2023-03-12 14:55:36 -0700

    corrected main.c

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -8,6 +8,7 @@
 
 int main(void)
 {
-	puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+	puts("\"At the end of the day, my goal was to be the best hacker\"\n\t"
+			"- Kevin Mitnick");
 	return (0);
 }
