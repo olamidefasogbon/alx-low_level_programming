837f0945f50f376b49eb5105df0d59f1dd7f4d48
Olamide <successolamide46@gmail.com>
2023-03-20 08:32:09 -0700

    Add: 2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
