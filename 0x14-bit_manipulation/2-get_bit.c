888681c9e81ce01447452fbf271545f360b0d1a9
Olamide <successolamide46@gmail.com>
2023-04-12 15:45:53 -0700

    Add: 2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,25 @@
+/*
+ * Name: Olamide
+ * File: 2-get_bit.c
+ * Date: 12th of April, 2023
+ */
+
+#include "main.h"
+
+/**
+ * get_bit - returns the value of a bit at an index in a decimal number
+ * @n: number to search
+ * @index: index of the bit
+ *
+ * Return: value of the bit
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int bit_val;
+
+	if (index > 63)
+		return (-1);
+	bit_val = (n >> index) & 1;
+	return (bit_val);
+}
