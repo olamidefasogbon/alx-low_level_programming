bdfcdbe36a47a1801e504888da82d6c8a8c2d51a
Olamide <successolamide46@gmail.com>
2023-04-12 15:59:07 -0700

    Add: 5-flips_bits.c

---
+++ 0x14-bit_manipulation/5-flips_bits.c
@@ -0,0 +1,31 @@
+/*
+ * Name: Olamide
+ * File: 5-flip_bits.c
+ * Date: 12th of April, 2023
+ */
+
+#include "main.h"
+
+/**
+ * flip_bits - counts the number of bits to change
+ * to get from one number to another
+ * @n: first number
+ * @m: second number
+ *
+ * Return: number of bits to change
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int i, count = 0;
+	unsigned long int current;
+	unsigned long int exclusive = n ^ m;
+
+	for (i = 63; i >= 0; i--)
+	{
+		current = exclusive >> i;
+		if (current & 1)
+			count++;
+	}
+	return (count);
+}
