5d932753e9e6ed0e39f41d9bfe2f4076cd1d198b
Olamide <successolamide46@gmail.com>
2023-04-12 15:16:16 -0700

    Add: 0-binary_to_unit.c

--- 0x14-bit_manipulation/0-binary_to_unit.c
+++ 0x14-bit_manipulation/0-binary_to_unit.c
@@ -1,28 +1,30 @@
+/*
+ * File_name: 0-binary_to_uint.c
+ * Date: 7th of April, 2023
+ * Name: Olamide
+ */
+
 #include "main.h"
 
 /**
- * binary_to_uint - converts a binary number to unsigned int.
- * @b: string containing the binary number.
- *
- * Return: the converted number.
+ * binary_to_uint - converts a binary number to an unsigned int.
+ * @b: a pointer a string of 0 and 1 chars.
+* Return: If b is NULL or contains chars not 0 or 1 - 0.
+ *         Otherwise - the converted number.
  */
 
 unsigned int binary_to_uint(const char *b)
 {
 	int i;
-	unsigned int dec_val = 0;
+	unsigned int brny = 0;
 
 	if (!b)
-	{
 		return (0);
-	}
 	for (i = 0; b[i]; i++)
 	{
 		if (b[i] < '0' || b[i] > '1')
-		{
 			return (0);
-		}
-		dec_val = 2 * dec_val + (b[i] - '0');
+		brny = 2 * brny + (b[i] - '0');
 	}
-	return (dec_val);
+	return (brny);
 }
