90f9b57f84c70cd525156b089b8f9c8acc008e98
Olamide <successolamide46@gmail.com>
2023-04-14 16:59:40 -0700

    Add: main.h

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * Name: Olamide
+ * File: main.h
+ * Date: 14th of April, 2023
+ */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif /*MAIN_H*/
